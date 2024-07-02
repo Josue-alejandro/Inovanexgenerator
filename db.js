3c2b03d1ad1a1e48f605e2ce73d4e4454da562b0
Josh <alejandromzv18@gmail.com>
2024-07-02 12:04:09 -0400

    new server

--- db.js
+++ db.js
@@ -9,6 +9,13 @@ const db = mysql.createConnection({
   database: 'playerad_dbradio',
 });
 
+//const db = mysql.createConnection({
+//  host: 'localhost',
+//  user: 'root',
+//  password: '',
+//  database: 'radiodb',
+//});
+
 db.connect((err) => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err);
